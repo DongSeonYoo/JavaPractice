7e253b7b98e3bfc505b86c4674de751c977094ce
YooDongSeon <inko51366@naver.com>
2022-10-24 13:51:12 +0900

    객체지향 연습문제

--- JavaSimpleProject/JumpToJava/ObjectOriented/Q8.java
+++ JavaSimpleProject/JumpToJava/ObjectOriented/Q8.java
@@ -1,2 +1,32 @@
-package ObjectOriented;public class Q8 {
+package ObjectOriented;
+
+
+//  8. 다음의 코드를 보고 1번, 2번, 3번, 4번, 5번 문장 중에서 오류가 발생하는 문장을 모두 찾고 오류의 원인에 대해서 설명하시오.
+//
+//  interface Predator {
+//  }
+//
+//  class Animal {
+//  }
+//
+//  class Dog extends Animal {
+//  }
+//
+//  class Lion extends Animal implements Predator {
+//  }
+//
+//  public class Sample {
+//      public static void main(String[] args) {
+//          Animal a = new Animal();  // 1번
+//          Animal b = new Dog();  // 2번
+//          Animal c = new Lion();  // 3번
+//          Dog d = new Animal();  // 4번 -< 오류
+//          Predator e = new Lion();  // 5번
+//      }
+//  }
+//
+//  이유 : Animal은 Dog의 부모클래스이기 때문. 상위클래스를 참조하지 못한다.
+
+public class Q8 {
+
 }
--- JavaSimpleProject/JumpToJava/ObjectOriented/Q9.java
+++ JavaSimpleProject/JumpToJava/ObjectOriented/Q9.java
@@ -1,2 +1,43 @@
-package ObjectOriented;public class Q9 {
+package ObjectOriented;
+
+//9. 다음의 코드를 보고 1번, 2번, 3번, 4번, 5번, 6번 문장 중에서 오류가 발생하는 문장을 모두 찾고 오류의 원인에 대해서 설명하시오.
+
+
+//  interface Predator {
+//      String bark();
+//  }
+//
+//  abstract class Animal {
+//      public String hello() {
+//          return "hello";
+//      }
+//  }
+//
+//  class Dog extends Animal {
+//
+//  }
+//
+//  class Lion extends Animal implements Predator {
+//      public String bark() {
+//          return "Bark bark!!";
+//      }
+////  }
+//
+//  public class Sample {
+//      public static void main(String[] args) {
+//          Animal a = new Lion();
+//          Lion b = new Lion();
+//          Predator c = new Lion();
+//
+//          System.out.println(a.hello());  // 1번
+//          System.out.println(a.bark());   // 2번    <- 오류   이유: Animal로 생성된 객체(hello메소드만 사용가능)
+//          System.out.println(b.hello());  // 3번
+//          System.out.println(b.bark());   // 4번
+//          System.out.println(c.hello());  // 5번    <- 오류   이유: Predator로 생성된 객체(bark메소드만 사용가능)
+//          System.out.println(c.bark());   // 6번
+//      }
+//  }
+
+public class Q9 {
+
 }
